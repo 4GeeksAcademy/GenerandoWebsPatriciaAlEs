90cf45115bba81d503385c50f799861e226fd691
PatriciaAlEs <168550797+PatriciaAlEs@users.noreply.github.com>
2024-07-10 09:56:46 +0000

    GenerandoWebsPatriciaAlEs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const pronombre = ['the', 'our'];
+const adjetivo = ['great', 'big'];
+const nombre = ['jogger', 'racoon'];
+const dominio = [".com", ".net",".es", ".eu"];
+
+for (let i = 0; i < pronombre.length; i++) {
+for (let a = 0; a < adjetivo.length; a++) {
+  for (let b = 0; b < nombre.length; b++) {
+    for (let c = 0; c < dominio.length; c++) {
+      console.log(pronombre[i]+ adjetivo[a] 
+  + nombre[b] + dominio[c]);
+    }
+    
+  }
+  
+}
+
+}
